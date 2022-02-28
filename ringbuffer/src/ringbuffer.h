b125e1e5ae513ab324856fe7ce0c9de15a8e1541
ocdal <alexander.bjornsrud@orangecyberdefense.com>
2022-02-28 13:59:52 +0100

    fixed build

--- ringbuffer/src/ringbuffer.h
+++ ringbuffer/src/ringbuffer.h
@@ -5,8 +5,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <dbg.h>
-#include "bstrlib.h"
+#include <c/dbg.h>
+#include <c/bstring/bstrlib.h>
 
 typedef struct {
 	bstring buffer;
