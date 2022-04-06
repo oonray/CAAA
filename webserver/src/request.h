0de775230bd744a244a2ccde1c14b121f85f608b
ocdal <alexander.bjornsrud@orangecyberdefense.com>
2022-04-06 16:14:19 +0200

    .

--- webserver/src/request.h
+++ webserver/src/request.h
@@ -1,9 +1,9 @@
 #ifndef REQUEST_H_
 #define REQUEST_H_
 
+#include "bstrlib.h"
 #include "fileio.h"
 #include "map.h"
-
 //---------------------------
 // Request
 // --------------------------
@@ -20,13 +20,35 @@ typedef struct Request {
 } Request;
 
 Request *Request_New(bstring data);
+void Request_Destroy(Request *req);
+bstring Request_To_String(Request *req);
 
 //---------------------------
 // Response
 // --------------------------
 
+//
+//        Response      = Status-Line               ; Section 6.1
+//                     *(( general-header        ; Section 4.5
+//                      | response-header        ; Section 6.2
+//                      | entity-header ) CRLF)  ; Section 7.1
+//                     CRLF
+//                     [ message-body ]          ; Section 7.2
+
 typedef struct Response {
+  // Status-Line = HTTP-Version SP Status-Code SP Reason-Phrase CRLF
+  bstring version;
+  int status_code;
+  bstring reason_phrase;
+  bstring status_line;
 
 } Response;
+
+// Fill in the fields that can be fetched from the request and return.
+Response *Response_New(Request *req, int status);
+void Response_Destroy(Request *req);
+bstring Response_To_String(Response *rsp);
+int Response_Set_Status(Response *req, int status);
+bstring Response_Status_TXT(int code);
 
 #endif // REQUEST_H_
--- webserver/tests/request_tests.c
+++ webserver/tests/request_tests.c
@@ -23,7 +23,55 @@ MunitResult test_new(const MunitParameter params[],
            bdata(r->uri));
 
   bstring host = Map_Get(r->Headers, bfromcstr("Host"));
-  log_info("Host: %s", bdata(host));
+  check(bstrcmp(host, bfromcstr("localhost")), "got Wrong value %s",
+        bdata(host));
+
+  check(bstrcmp(r->body,
+                bfromcstr(
+                    "This is the boddy of christ.\nAlso this is his blood.")) ==
+            0,
+        "Got wrong body: \n------------\n%s\n------------\n", bdata(r->body));
+
+  return MUNIT_OK;
+error:
+  return MUNIT_FAIL;
+}
+
+MunitResult test_code(const MunitParameter params[],
+                      void *user_data_or_fixture) {
+  // 404
+  bstring code = Response_Status_TXT(404);
+  check(bstrcmp(code, bfromcstr("Not Found")) == 0,
+        "Wrong response text recieved");
+  log_info("%s", bdata(code));
+
+  code = Response_Status_TXT(200);
+  check(bstrcmp(code, bfromcstr("OK")) == 0, "Wrong response text recieved");
+  return MUNIT_OK;
+error:
+  return MUNIT_FAIL;
+}
+
+MunitResult test_response(const MunitParameter params[],
+                          void *user_data_or_fixture) {
+  ioStream *req = NewIoStreamFile(bfromcstr("../webserver/tests/request.req"),
+                                  O_RDWR, 0644, 1024 * 10);
+  check(IoStreamIoRead(req) > 0, "Could not read request");
+
+  log_info("Loading Request");
+  Request *r = Request_New(RingBuffer_Get_All(req->in));
+  check(r != NULL, "Error in getting Request");
+
+  // Request GOT
+  // Default
+  Response *rsp = Response_New(r, 0);
+  check(rsp != NULL, "error when creating request");
+  check(rsp->status_code == 200, "wrong code got when expecting 200");
+  check(bstrcmp(rsp->reason_phrase, bfromcstr("OK")) == 0,
+        "wrong reason got %s", bdata(rsp->reason_phrase));
+  check(bstrcmp(rsp->version, r->version) == 0, "Wrong version returned");
+
+  log_info("%s", bdata(rsp->status_line));
 
   return MUNIT_OK;
 error:
@@ -33,6 +81,11 @@ error:
 int main(int argc, char *argv[]) {
   MunitTest tests[] = {
       {" requests tests", test_new, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+      {" response code test", test_code, NULL, NULL, MUNIT_TEST_OPTION_NONE,
+       NULL},
+
+      {" response test", test_response, NULL, NULL, MUNIT_TEST_OPTION_NONE,
+       NULL},
       {NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}};
 
   const MunitSuite suite = {"Request Tests", tests, NULL, 1,
