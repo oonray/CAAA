5960e6334034c3f8d1ecdf7ad624c6b139d25376
oonray <alexanderbjornsrud@gmail.com>
2023-09-07 14:42:01 +0200

    .

--- caaa.rb
+++ caaa.rb
@@ -4,7 +4,7 @@
 class Caaa < Formula
   desc "c with aaa batteres"
   homepage ""
-  url "https://github.com/oonray/CAAA/archive/refs/tags/v1.1.20.tar.gz"
+  url "https://github.com/oonray/CAAA/archive/refs/tags/v1.1.21.tar.gz"
   license "MIT"
 
   depends_on "cmake" => :build
