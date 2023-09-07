b2dbd12a06fa41b3210252a26f023f2d86f7ec3d
oonray <alexanderbjornsrud@gmail.com>
2023-09-07 14:41:38 +0200

    .

--- dbg/ca_logging.h
+++ dbg/ca_logging.h
@@ -7,19 +7,23 @@
 #define open_log(N, O, P) openlog(N, O, P)
 #define close_log() closelog()
 
+#undef DBG_FORMAT
 #define DBG_FORMAT(S, M, P) "[file: %s | func %s | line %d] [" P "] " M "\n"
 
 #define dbg_syslog(S, P, M, ...)                                               \
   syslog(S, DBG_FORMAT(M, P), __FILE__, __func__, __LINE__, ##__VA_ARGS__);
 
+#undef log_info
 #define log_info(M, ...)                                                       \
   { dbg_syslog(LOG_INFO, "NULL", M, ##__VA_ARGS__); }
 
+#undef log_error
 #define log_error(M, ...)                                                      \
   {                                                                            \
     dbg_syslog(LOG_ERR, "NULL", "errno | %d | %s", M, errno, dbg_get_errno(),  \
                ##__VA_ARGS__);
 
+#undef check
 #define check(A, M, ...)                                                       \
   {                                                                            \
     if (!(A)) {                                                                \
@@ -29,5 +33,6 @@
     }                                                                          \
   }
 
+#undef check_memory
 #define check_memory(A) check((A), "Memory Error")
 #endif // CA_LOGGING_H_
